18dd84daab8875d157d7968e26fca327f6e45cfa
prathyushaband <prathyushabandi0@gmail.com>
2023-05-05 11:44:05 +0530

    sending code

---
+++ Student.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Student data");
+        List<String> m = new ArrayList<>();
+
+    }
+}
